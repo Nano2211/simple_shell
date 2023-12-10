8c192461d3002bb9e81c2c1f0ea97f1198dcb326
Mihlali Nene <drmnene@gmail.com>
2023-12-10 09:28:50 -0800

    ei its heavy

---
+++ builtin.c
@@ -0,0 +1,94 @@
+#include "shell.h"
+
+/**
+ * _myexit - func that exits shell
+ * @info: Struct with arguments
+ * Return: whatever needs to be returned
+ * (0) if info.argv[0] != "exit"
+ */
+int _myexit(info_t *info)
+{
+	int check_exit;
+
+	if (info->argv[1]) /* if the vector exits */
+	{
+		checkexit = _erratoi(info->argv[1]);
+		if (checkexit == -1)
+		{
+			info->status = 2;
+			print_error(info, "Wrong number: ");
+			_eputs('\n');
+			return (1);
+		}
+		info->err_num = _erratoi(info->argv[1]);
+		return (-2);
+	}
+	info->err_num = -1;
+	return (-2);
+}
+
+/**
+ * _mycd - changes working directory process
+ * @info: changes shel state
+ * Return: Always returns a value of 0
+ */
+int _mycd(info_t *info)
+{
+	char *s, *dir, buffer[1024];
+	int chdir_ret;
+
+	s = getcwd(buffer, 1024);
+	if (!s)
+		_puts("TODO: >>ERROR!<<\n");
+	if (!info->argv[1])
+	{
+		dir = _getenv(info, "HOME=");
+		if (!dir)
+			chdir_ret = /*Kobai what should this be? */
+				chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
+		else
+			chdir_ret = chdir(dir);
+	}
+	else if (_strcmp(info->argv[1], "-") == 0)
+	{
+		if (!_getenv(info, "OLDPWD="))
+		{
+			_puts(s);
+			_putchar('\n');
+			return (1);
+		}
+		_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
+		chdir_ret = /* help sensei */
+			chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
+	}
+	else
+		chdir_ret = chdir(info->argv[1]);
+	if (chdir_ret == -1)
+	{
+		print_error(info, "can't cd to ");
+		_eputs(info->argv[1]), _eputchar('\n');
+	}
+	else
+	{
+		_setenv(info, "OLDPWD", _getenv(info, "PWD="));
+		_setenv(info, "PWD", getcwd(buffer, 1024));
+	}
+	return (0);
+}
+
+/**
+ * _myhelp - changes the current directory.
+ * @info: Structure containing potential arguments. Used to maintain
+ * constant function prototype.
+ * Return: Always returns a val of 0
+ */
+int _myhelp(info_t *info)
+{
+	char **arg_array;
+
+	arg_array = info->argv;
+	_puts("Funciton inop \n");
+	if (0)
+		_puts(*arg_array); /* bandage on a wound */
+	return (0);
+}
